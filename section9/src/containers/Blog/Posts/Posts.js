c547c3bb286db5a2157488227b94bbe4f7d806fa
Morne Louw <louw.morne@gmail.com>
2019-08-13 23:27:38 +0200

    Restored a nice comment I previously made

--- section9/src/containers/Blog/Posts/Posts.js
+++ section9/src/containers/Blog/Posts/Posts.js
@@ -14,6 +14,17 @@ class Posts extends Component {
         this.setState({selectedPostId: id});
     }
 
+    // This does an Ajax call (HTTP GET request)
+    // As it is an Asynchronous request React will continue on to the next line of code so that the Application
+    // does not have to freeze and become not-interactive while the data is fetched, which is a benefit, BUT
+    // this also means that the axios.get() function cannot return data into a variable.
+    // To get to the return data you have to have a separate process that sits and waits for it until it returns while
+    // the rest of the application continues so long.
+    // This is done through JS promises and the axios function for using this promise (waiting for the returned data)
+    // if the "then()", which id simply chained to the .get() function
+    // The .then() function automatically receives a JSON data object, which you can name anything, but is commonly
+    // named "response"
+    // This response JSON object must then be passed into a function as a parameter so you can do what you need to with it
     componentDidMount() {
         axios.get('/posts')
             .then(response => {
