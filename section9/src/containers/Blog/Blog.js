7839dde24da95a0d356747775b815db8cb23c41e
Morne Louw <louw.morne@gmail.com>
2019-08-19 23:29:39 +0200

    Continued with Routing. Dynamic Routing

--- section9/src/containers/Blog/Blog.js
+++ section9/src/containers/Blog/Blog.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from "./NewPost/NewPost";
 
 import './Blog.css';
-
-// Testing Version Control via phpStorm TODO delete this line
 
 class Blog extends Component {
     render() {
@@ -15,7 +13,7 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink to="/" exact>Home</NavLink></li>
+                            <li><NavLink to="/posts/" exact>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: "/new-post",
                                 hash: "#submit",
@@ -26,8 +24,10 @@ class Blog extends Component {
                 </header>
                 {/*<Route path="/" exact render={() => <h1>Home</h1>}/>
                 <Route path="/" render={() => <h1>Home 2</h1>}/>*/}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
+                <Switch>
+                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/posts" component={Posts}/>
+                </Switch>
             </div>
         );
     }
--- section9/src/containers/Blog/Posts/Posts.js
+++ section9/src/containers/Blog/Posts/Posts.js
@@ -1,27 +1,25 @@
 import React, {Component} from 'react';
+import { Route } from "react-router-dom";
 import axios from "axios";
+
 import Post from '../../../components/Post/Post';
+import FullPost from "../FullPost/FullPost";
+
 import './Posts.css';
 
 class Posts extends Component {
     state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+        posts: []
     }
 
     // This does an Ajax call (HTTP GET request)
     // As it is an Asynchronous request React will continue on to the next line of code so that the Application
-    // does not have to freeze and become not-interactive while the data is fetched, which is a benefit, BUT
+    // does not have to freeze and become non-interactive while the data is being fetched, which is a benefit, BUT
     // this also means that the axios.get() function cannot return data into a variable.
     // To get to the return data you have to have a separate process that sits and waits for it until it returns while
-    // the rest of the application continues so long.
-    // This is done through JS promises and the axios function for using this promise (waiting for the returned data)
-    // if the "then()", which id simply chained to the .get() function
+    // the rest of the application continues.
+    // This is done through JS promises and the axios-function for using this promise (waiting for the returned data)
+    // is the "then()" function, which is simply chained to the .get() function
     // The .then() function automatically receives a JSON data object, which you can name anything, but is commonly
     // named "response"
     // This response JSON object must then be passed into a function as a parameter so you can do what you need to with it
@@ -45,22 +43,32 @@ class Posts extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.props.history.push({pathname: '/posts/' + id});
+    }
+
     render() {
         let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}/>;
+                return (
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}
+                    />
+                );
             });
         }
 
         return (
-            <section className="Posts">
-                {posts}
-            </section>
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url + "/:id"} exact component={FullPost} />
+            </div>
         )
     }
 }
